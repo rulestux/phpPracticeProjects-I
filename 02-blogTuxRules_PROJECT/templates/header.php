ee6ca14b8d26de2ab553ca2e79d4ebff60a55c0c
rulestux <rulestux@gmail.com>
2025-11-19 19:03:04 -0300

    Navigation Bar

--- 02-blogTuxRules_PROJECT/templates/header.php
+++ 02-blogTuxRules_PROJECT/templates/header.php
@@ -16,12 +16,23 @@ include_once("helpers/url.php");
     <!-- <link rel="stylesheet" href="styles.css"> -->
     <!-- chamada de arquivo CSS com PHP, utilizando o HELPER 'url.php': -->
     <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
-    <!-- GOOGLE FONT Montserrat -->
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-    </style>
+    <!-- GOOGLE FONT ROBOTO -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
-<header>
-    Template Header
-</header>
+    <header>
+        <a href="<?= $BASE_URL ?>" id="logo">
+            <img src="<?= $BASE_URL ?>/img/logo.png" alt="TuxRules Blog">
+        </a>
+        <!-- BARRA DE NAVEGAÇÃO -->
+        <nav>
+            <ul id="navbar">
+            <li><a href="<?= $BASE_URL ?>" class="nav-link">Home</a></li>
+            <li><a href="#" class="nav-link">Categories</a></li>
+            <li><a href="#" class="nav-link">About</a></li>
+            <li><a href="<?= $BASE_URL ?>contact.php" class="nav-link">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
